f90657212891be57168fd78ffc00473713ce30e8
Mohammad Mahdi <mahdi.boss2018@gmail.com>
2025-08-02 13:44:06 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,10 +4,22 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '1234')  # میشه از env هم خوند
     
     # اطلاعات database
-    db_inf = "root:1234@localhost:3306/project"
+    db_config = {
+        "user": "root",
+        "password": "1234",
+        "host": "localhost",
+        "port": 3306,
+        "database": "project"
+    }
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{db_inf}"
+        f"mysql+pymysql://{db_config['user']}:{db_config['password']}"
+        f"@{db_config['host']}:{db_config['port']}/{db_config['database']}"
     )
+    
     SESSION_COOKIE_SAMESITE = 'Lax'
     SESSION_COOKIE_SECURE = False
+
+
+
+
